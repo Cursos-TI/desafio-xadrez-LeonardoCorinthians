dcf54dd6b28b884858330367b18378fd9a463e7c
LeonardoCorinthians <leonardo_sampaio9@hotmail.com>
2025-10-31 21:58:48 -0300

    Atualizar o desafio-xadrez-LeonardoS.c
    atualizei commit

--- desafio-xadrez-LeonardoS.c
+++ desafio-xadrez-LeonardoS.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    // ==============================
-    // TORRE - usa estrutura FOR
-    // Move-se em linha reta (horizontal ou vertical)
-    // Aqui ela anda 5 casas para a direita
-    // ==============================
+  
+    
     int casasTorre = 5;
 
     printf("=== Movimento da TORRE ===\n");
@@ -13,11 +10,7 @@ int main() {
         printf("Casa %d: Direita\n", i);
     }
 
-    // ==============================
-    // BISPO - usa estrutura WHILE
-    // Move-se em diagonal
-    // Aqui ele anda 5 casas na diagonal "Cima e Direita"
-    // ==============================
+    
     int casasBispo = 5;
     int contadorBispo = 1;
 
@@ -27,11 +20,7 @@ int main() {
         contadorBispo++;
     }
 
-    // ==============================
-    // RAINHA - usa estrutura DO-WHILE
-    // Move-se em todas as direções
-    // Aqui ela anda 8 casas para a esquerda
-    // ==============================
+    /
     int casasRainha = 8;
     int contadorRainha = 1;
 
